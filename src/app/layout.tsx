e4e85699d57494258d79d071c633d9e37dc9ea33
anonlegionoke <anonlegionoke@protonmail.com>
2025-05-06 19:16:20 +0530

    misc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { PerformanceProvider } from "./context/PerformanceContext";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +32,8 @@ export default function RootLayout({
       >
         <PerformanceProvider>
           {children}
+          <Analytics />
+          <SpeedInsights />
         </PerformanceProvider>
       </body>
     </html>
